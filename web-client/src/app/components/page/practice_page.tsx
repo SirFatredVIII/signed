5736a50910c68308dc4321e629359904d6cab110
nvandrie <nvandrie2@huskers.unl.edu>
2025-03-27 01:13:55 -0500

    Webcam functionality

--- web-client/src/app/components/page/practice_page.tsx
+++ web-client/src/app/components/page/practice_page.tsx
@@ -1,17 +1,16 @@
 import { Button } from "@mui/material";
 import { Dispatch, SetStateAction, useState } from "react";
+import Webcam from "../webcam/webcam";
 
-interface HomePageProps {
-  
-} 
+interface HomePageProps {}
 
 export const PracticePage: React.FC<HomePageProps> = ({}) => {
-
-    return (
-      <>
-          <h1 className={"w-full flex justify-center text-7xl pt-20 italic font-bold select-none"}>
+  return (
+    <>
+      {/* <h1 className={"w-full flex justify-center text-7xl pt-20 italic font-bold select-none"}>
             Place giant fuck-ass camera frame here
-          </h1>
-      </>
-    );
-}
+          </h1> */}
+      <Webcam />
+    </>
+  );
+};
---
+++ web-client/src/app/components/webcam/webcam.tsx
@@ -0,0 +1,88 @@
+import { Button } from "@mui/material";
+import { useRef, useState } from "react";
+
+const Webcam = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
+  const [capturedImage, setCapturedImage] = useState<string | null>(null);
+
+  const startWebcam = async () => {
+    if (videoRef.current && canvasRef.current) {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
+        videoRef.current.srcObject = stream;
+        setMediaStream(stream);
+      } catch (error) {
+        console.error("Error accessing webcam:", error);
+      }
+    }
+  };
+
+  const stopWebcam = () => {
+    if (mediaStream) {
+      mediaStream.getTracks().forEach((track) => track.stop());
+      setMediaStream(null);
+    }
+  };
+
+  const captureImage = () => {
+    if (canvasRef.current && videoRef.current) {
+      const video = videoRef.current;
+      const canvas = canvasRef.current;
+      const context = canvasRef.current.getContext("2d");
+
+      // Set canvas dimensions to match video stream
+      if (context && video.videoWidth && video.videoHeight) {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        // Draw video frame onto canvas
+        context.drawImage(video, 0, 0);
+        // Get image data URL from canvas
+        setCapturedImage(canvas.toDataURL("image/png"));
+        stopWebcam();
+      }
+    }
+  };
+
+  const resetState = () => {
+    stopWebcam();
+    setCapturedImage(null);
+  };
+
+  return (
+    <div className="webcam-container flex flex-col items-center justify-center">
+      {capturedImage ? (
+        <>
+          <img src={capturedImage} alt="Captured" className="preview-img" />
+          <Button onClick={resetState} variant="contained" color="warning">
+            Reset
+          </Button>
+        </>
+      ) : (
+        <>
+          <video ref={videoRef} autoPlay muted className="webcam-video" />
+          <canvas ref={canvasRef} className="webcam-canvas" />
+          {!videoRef.current ? (
+            <Button onClick={startWebcam} variant="contained" color="primary">
+              Start Webcam
+            </Button>
+          ) : (
+            <Button
+              onClick={captureImage}
+              variant="contained"
+              color="secondary"
+            >
+              Capture Image
+            </Button>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Webcam;

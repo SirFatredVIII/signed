657a983e5d98c93d41879a7e2b674d47f8df0209
nvandrie <nvandrie2@huskers.unl.edu>
2025-03-26 22:21:54 -0500

    Set up express server

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/api", (_, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});

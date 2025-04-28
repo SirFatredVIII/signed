69748c99ab0d0863c9bd7d5bacbb61ceacec82a6
SirFatredVIII <dhall25@huskers.unl.edu>
2025-04-28 16:26:02 -0500

    initialize firebase
    * firebase console initialized, we'll have to add developers to it though
    * firebase configuration file created
    * installed firebase, react-firebase-hooks (which'll be useful later for getting the authenticated state)
    + added a short example in a useEffect hook to show how to access documents / collections in firebase

--- web-client/src/app/page_switcher.tsx
+++ web-client/src/app/page_switcher.tsx
@@ -1,10 +1,23 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { HomePage } from "./pages/home/home";
 import { PracticePage } from "./pages/practice/practice";
-
+import { config } from "../../configuration";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
 export default function PageSwitcher() {
   const [page, setPage] = useState("home");
+
+  useEffect(() => {
+    // Initialize the Firebase database with the provided configuration
+    const database = getFirestore(config);
+    
+    // Reference to the admin document in the database.
+    // TODO: This shit's gotta go, man, but it's a good example for now of how to access collections / documents
+    const adminAccount = getDoc(doc(database, "users", "admin_0"));
+    adminAccount.then((data) => {
+      console.log(data.data())
+    })
+  });
 
   return (
     <>

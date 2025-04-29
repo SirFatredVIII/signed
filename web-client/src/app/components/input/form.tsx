a33e39078bddacd7c4a49c1d5ab21b50fa5c99d0
SirFatredVIII <dhall25@huskers.unl.edu>
2025-04-28 23:55:06 -0500

    added form verification
    + Added submit button
    + Added error messages to ensure form elements are good

--- web-client/src/app/components/input/form.tsx
+++ web-client/src/app/components/input/form.tsx
@@ -5,16 +5,19 @@ interface InputFormProps {
     label: string;
     value: string;
     callback: (_: BaseSyntheticEvent<object, any, any>) => void;
+    errorMessage: string;
+    isError: boolean;
 }
 
 /**
  * A simple wrapper for an input form that looks quite pleasant.
  */
-export const InputForm: React.FC<InputFormProps> = ({type, label, value, callback}) => {
+export const InputForm: React.FC<InputFormProps> = ({type, label, value, callback, errorMessage, isError}) => {
     return (
         <div className="grid justify-center mb-5 w-10vw">
             <label htmlFor={label} className="text-center text-signed-dark-blue font-bold">{label}</label>
             <input id={label} type={type} onChange={callback} value={value} className="border-5 border-signed-blue rounded-md pl-2 pr-2 outline-0 outline-signed-dark-blue focus:outline-2 bg-white"/>
+            {isError && <div className="text-red-900 text-center">{errorMessage}</div>}
         </div>
     )
 }
--- web-client/src/app/pages/auth/signUp.tsx
+++ web-client/src/app/pages/auth/signUp.tsx
@@ -1,6 +1,6 @@
 import { InputButton } from "@/app/components/input/button";
 import { InputForm } from "@/app/components/input/form";
-import { BaseSyntheticEvent, useContext, useState } from "react"
+import { BaseSyntheticEvent, useContext, useEffect, useState } from "react"
 import { StateContext } from "../../../../context";
 
 /**
@@ -16,42 +16,140 @@ export const SignUp = () => {
     const [confirmPassword, setConfirmPassword] = useState("");
 
     const {state, setState} = useContext(StateContext);
+
+    const [triedSignUp, setTriedSignUp] = useState(false);
     
     /**
      * A list of all input forms that make up the sign-up sheet. Should be quite exapandable if we ever need more.
      */
-    const allForms: { type: "text" | "email" | "password", label: string, value: string, handler: (_: BaseSyntheticEvent) => void }[] = [
+    let allForms: { 
+        type: "text" | "email" | "password", 
+        label: string, 
+        value: string, 
+        handler: (_: BaseSyntheticEvent) => void,
+        errorCondition: () => boolean, 
+        errorMessage: string,
+        error: boolean}[] = [
         {
             type: "text",
             label: "Username",
             value: username.toString(),
-            handler: (e: BaseSyntheticEvent) => setUsername(e.target.value)
+            handler: (e: BaseSyntheticEvent) => setUsername(e.target.value),
+            errorCondition: () => false,
+            errorMessage: "na",
+            error: false
         },
         {
             type: "email",
             label: "Email Address",
             value: email.toString(),
-            handler: (e: BaseSyntheticEvent) => setEmail(e.target.value)
+            handler: (e: BaseSyntheticEvent) => setEmail(e.target.value),
+            errorCondition: () => {return !email.includes("@")},
+            errorMessage: "Not a valid email address!",
+            error: false
         },
         {
             type: "email",
             label: "Confirm Email Address",
             value: confirmEmail.toString(),
-            handler: (e: BaseSyntheticEvent) => setConfirmEmail(e.target.value)
+            handler: (e: BaseSyntheticEvent) => setConfirmEmail(e.target.value),
+            errorCondition: () => {return email !== confirmEmail},
+            errorMessage: "Emails don't match!",
+            error: false
         },
         {
             type: "password",
             label: "Password",
             value: password.toString(),
-            handler: (e: BaseSyntheticEvent) => setPassword(e.target.value)
+            handler: (e: BaseSyntheticEvent) => setPassword(e.target.value),
+            errorCondition: () => {return false},
+            errorMessage: "na",
+            error: false
         },
         {
             type: "password",
             label: "Confirm Password",
             value: confirmPassword.toString(),
-            handler: (e: BaseSyntheticEvent) => setConfirmPassword(e.target.value)
+            handler: (e: BaseSyntheticEvent) => setConfirmPassword(e.target.value),
+            errorCondition: () => {return password !== confirmPassword},
+            errorMessage: "Passwords don't match!",
+            error: false
         }
     ]
+
+    if (triedSignUp) {
+            let errorFound = false;
+
+        const newForms: { 
+            type: "text" | "email" | "password", 
+            label: string, 
+            value: string, 
+            handler: (_: BaseSyntheticEvent) => void,
+            errorCondition: () => boolean, 
+            errorMessage: string,
+            error: boolean}[] = []
+
+        allForms.forEach((form) => {
+            if (form.errorCondition()) {
+                errorFound = true;
+
+                let newForm = {...form, error: true};
+
+                newForms.push(newForm);
+            } else {
+                newForms.push(form);
+            }
+        })
+
+        allForms = newForms;
+
+    }
+
+    /**
+     * Event handler designed to renavigate to the sign in page.
+     */
+    const handleSignIn = () => {
+        // setState({...state, currentPage: "signIn"});
+        handleSignUp();
+    }
+
+    /**
+     * Validates the inputs received, creates a new Firebase user entry, and navigates to the sign in page.
+     */
+    const handleSignUp = () => {
+        setTriedSignUp(false);
+        let errorFound = false;
+
+        const newForms: { 
+            type: "text" | "email" | "password", 
+            label: string, 
+            value: string, 
+            handler: (_: BaseSyntheticEvent) => void,
+            errorCondition: () => boolean, 
+            errorMessage: string,
+            error: boolean}[] = []
+            
+        allForms.forEach((form) => {
+            if (form.errorCondition()) {
+                errorFound = true;
+
+                let newForm = {...form, error: true};
+
+                newForms.push(newForm);
+            } else {
+                newForms.push(form);
+            }
+        })
+
+        if (errorFound) {
+            setTriedSignUp(true);
+            allForms = newForms;
+        } else {
+            // add new entry to firebase here
+            // setState({...state, currentPage: "signIn"});
+            console.log("no errors!")
+        }
+    }
 
     // we'll show the other forms if the previous form has been typed into
     const currentForms = [allForms[0]];
@@ -61,12 +159,8 @@ export const SignUp = () => {
         }
     }
 
-    /**
-     * Event handler designed to renavigate to the sign in page.
-     */
-    const handleSignIn = () => {
-        setState({...state, currentPage: "signIn"});
-    }
+    // if all forms are filled out, feel free to submit the form. or at least try.
+    const readyToSubmit = confirmPassword.length > 3;
 
     return (
         <div className="pt-15">
@@ -75,15 +169,16 @@ export const SignUp = () => {
                     <h2 className={"w-full flex justify-center text-3xl font-bold select-none text-white bg-signed-blue p-2 mb-10"}>
                         Sign Up
                     </h2>
-                    <div className="w-full grid justify-center">
+                    <div className="w-full grid justify-center mb-5">
                         {
                             currentForms.map((form) => {
                                 return (
-                                    <InputForm type={form.type} label={form.label} callback={form.handler} value={form.value} key={form.label}/>
+                                    <InputForm type={form.type} label={form.label} callback={form.handler} value={form.value} key={form.label} errorMessage={form.errorMessage} isError={form.error}/>
                                 )
                             })
                         }
                     </div>
+                    {readyToSubmit && <InputButton color="blue" label="Sign up" callback={handleSignUp}></InputButton>}
                 </div>
             </div>
             <div className="grid justify-center mb-5 w-10vw ">

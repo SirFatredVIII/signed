27396bf30599cc0419caee22ed4caf664802fd87
carlykendeigh <carlykendeigh@gmail.com>
2025-04-30 22:06:22 -0500

    home page nearly done

--- web-client/src/app/layout.tsx
+++ web-client/src/app/layout.tsx
@@ -1,17 +1,24 @@
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Barlow } from "next/font/google";
 import "./globals.css";
 import FooterBar from "./layouts/footer_bar/footer_bar";
 import { HeaderBar } from "./layouts/headerBar/headerBar";
+import type { Metadata } from "next";
+
+const barlow = Barlow({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-barlow",
+});
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
   subsets: ["latin"],
+  variable: "--font-geist-sans",
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
+  variable: "--font-geist-mono",
 });
 
 export const metadata: Metadata = {
@@ -21,14 +28,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${barlow.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}>
         <HeaderBar />
         {children}
         {/* <FooterBar /> */}
--- web-client/src/app/pages/home/home.tsx
+++ web-client/src/app/pages/home/home.tsx
@@ -2,22 +2,21 @@
 import { Button } from "@mui/material";
 import { Dispatch, SetStateAction } from "react";
 import Image from "next/image";
-import styles from "./home.module.css"; // custom class import
+import styles from "./home.module.css";
+import signedLogo from "./signedLogo.png";
+
 
 interface HomePageProps {
   setPage: Dispatch<SetStateAction<string>>;
 }
 
 export const HomePage: React.FC<HomePageProps> = ({ setPage }) => {
-  const onClick = () => {
-    setPage("practice");
-  };
 
   return (
     <div className="flex flex-col items-center w-full bg-white text-[#1D203E]">
       {/* Top Bar */}
       <div className={styles.topBar}>
-        <Image src="/signed.png" alt="SignEd Logo" width={319} height={84} />
+        <Image src={signedLogo} alt="SignEd Logo" width={319} height={84} />
         <div className={styles.navText}>
           <p>About Us</p>
           <p>Donate</p>
@@ -29,7 +28,9 @@ export const HomePage: React.FC<HomePageProps> = ({ setPage }) => {
       <div className={styles.heroContainer}>
         <h1 className={styles.heroText}>Learn to sign on your own time</h1>
 
-        <Button className={styles.heroButton}>SignEd Learning Software </Button>
+        <Button className={styles.heroButton} onClick={() => setPage("signup")}>
+          SignEd Learning Software
+        </Button>
 
         <p className="text-[60px] text-black">SignEd is free for all users</p>
       </div>
